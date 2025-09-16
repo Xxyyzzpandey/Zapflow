8bddb25f241c8d38883bc2f30a710ec9111b9a6c
ankit <ujjawalankit56023@gmail.com>
2025-09-16 19:05:21 +0530

    changed schema and modified index file

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -8,29 +8,32 @@ const client=new PrismaClient();
 app.use(express.json());
 
 //https://hooks.zapier.com/hooks/catch/223345/848489/
-//pasword logic
 
-app.post("/hooks/catch/:userId/:zapId", async(req,res)=>{
-    const userId=req.params.userId;
-    const zapId=req.params.zapId;
-    const body =req.body
-    
-    //store in db a new trigger
-    await client.$transaction(async tx=>{
-        const run=await tx.zapRun.create({
-            data:{
-                zapId:zapId,
-                metadata:body
-            }
-        });
 
-        await tx.zapRunOutbox.create({
-            data:{
-                zapRunId:run.id
-            }
-        })
-    })
-})
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+  const { userId, zapId } = req.params;
+  const body = req.body;
+
+  await client.$transaction(async tx => {
+    const run = await tx.zapRun.create({
+      data: {
+        zapId,
+        metadata: body
+      }
+    });
+
+    await tx.zapRunOutbox.create({
+      data: {
+        zapRunId: run.id
+      }
+    });
+  }, { timeout: 10000,
+       maxWait: 5000
+   });
+
+  res.json({ success: true, zapId, userId, received: body });
+});
+
 
 
 app.listen(3002,()=>{console.log("server is running at port 3002...")})
