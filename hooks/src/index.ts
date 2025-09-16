9d9e22fde02aa44432772c06bcdc0211464f3e04
ankit <ujjawalankit56023@gmail.com>
2025-09-16 19:28:02 +0530

    pd2

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -9,11 +9,12 @@ app.use(express.json());
 
 //https://hooks.zapier.com/hooks/catch/223345/848489/
 
-
+//@ts-ignore
 app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
   const { userId, zapId } = req.params;
   const body = req.body;
 
+  //@ts-ignore
   await client.$transaction(async tx => {
     const run = await tx.zapRun.create({
       data: {
