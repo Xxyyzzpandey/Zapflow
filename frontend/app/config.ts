0e5efcf212ac67db2c72a1f8fd79d1aa16122923
ankit <ujjawalankit56023@gmail.com>
2025-09-17 19:27:26 +0530

    changed urls

--- frontend/app/config.ts
+++ frontend/app/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://localhost:3000"
-export const HOOKS_URL = "http://localhost:3002"
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL
+export const HOOKS_URL = process.env.NEXT_PUBLIC_HOOKS_URL;
